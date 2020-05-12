c4bdd4483fa23bf4b0b652fb13bd284deb5ab055
emmanueljucaban <emmanueljucaban@gmail.com>
2020-05-12 10:19:13 -0700

    Push to git

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+mongoose.connect(process.env.MONGODB_URI  || 'mongodb://localhost/redux_todo_db');
+app.listen(PORT);
