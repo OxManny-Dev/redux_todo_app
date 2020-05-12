ccebdfc45699660cdc74fa8a0fc350cd18d3b4e5
emmanueljucaban <emmanueljucaban@gmail.com>
2020-05-12 12:16:47 -0700

    Update repo

--- models/User.js
+++ models/User.js
@@ -1,23 +1,23 @@
 const { Schema, model } = require('mongoose');
-  const { isEmail, isLength } = require('validator');
+const { isEmail, isLength } = require('validator');
 
-  const UserSchema = new Schema({
-    email: {
-      type: String,
-      unique: true,
-      lowercase: true,
-      validate: [isEmail, 'Please enter a valid email address'],
-      required: [true, 'You must provide an email address']
-    },
-    password: {
-      type: String,
-      required: [true, 'You must provide a password'],
-      validate: [ (value) => isLength(value, { min: 6 }), 'Your password must be at least 6 characters long' ]
-    },
-    dateCreated: {
-      type: Date,
-      default: Date.now()
-    }
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email address'],
+    required: [true, 'You must provide an email address']
+  },
+  password: {
+    type: String,
+    required: [true, 'You must provide a password'],
+    validate: [ (value) => isLength(value, { min: 6 }), 'Your password must be at least 6 characters long' ]
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now()
+  }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = model('User', UserSchema);
--- routes/apiRoutes/authRoutes/index.js
+++ routes/apiRoutes/authRoutes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
+const { signUp } = require('./../../../controllers/authController');
 
-router.post('/signup');
+router.post('/signup', signUp);
 
 module.exports = router;
